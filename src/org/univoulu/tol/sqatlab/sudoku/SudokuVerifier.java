8705af068fceeb5bdf3279c293c1cd936557403e
somename <someemail>
2015-10-02 12:36:09 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -9,15 +9,15 @@ public class SudokuVerifier {
 		//if(candidateSolution.length()!=81)
 		//	return -5;
 		
-		char[] solluctionArray = candidateSolution.toCharArray();
-		String tmp = candidateSolution.substring(0, 9);
-		
+		char[] solluctionArray = candidateSolution.toCharArray();		
 		if(!correctNumbers(solluctionArray)) {
 			return -1;
 		}
 		
+		
+		String tmp = candidateSolution.substring(0, 9);
 		if(!unityCorrect(tmp.toCharArray()))
-			return -2;
+			return -3;
 		return 0;		
 	}
 	
@@ -32,19 +32,12 @@ public class SudokuVerifier {
 		return true;
 	}
 	
-	public boolean correctNumbers(char [] tmp) {
-		int [] tmpI = new int[tmp.length];
-		for (int i=0; i<tmpI.length;i++) {
-			tmpI[i]= tmp[i];
-		}
-		
+	public boolean correctNumbers(char [] tmp) {	
 		for (int i=0; i<tmp.length; i++) {
-			System.out.println((int)(tmp[i]));
 			if((int)(tmp[i])<48 || (int)(tmp[i])>56) {
-				System.out.println(tmp[i]);
 				return false;
 			}
-			}				
+		}				
 		return true;
 	}
 }
