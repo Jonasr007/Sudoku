c7f4b3711e3ee10012d117964e0583422e888f19
somename <someemail>
2015-10-02 12:58:47 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -21,7 +21,7 @@ public class SudokuVerifier {
 		}
 		
 		for (int i=0; i<subgrids.length; i++) {
-			String tmp = candidateSolution.substring(0+10*i,9+10*i);
+			String tmp = candidateSolution.substring(0+9*i,9+9*i);
 		//	System.out.println(tmp);
 			if(!unityCorrect(tmp.toCharArray()))
 				return -3;
